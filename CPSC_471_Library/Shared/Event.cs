278328da620099ddbabb7682bf872cf322e24ae6
bonnie-wu <bonnie.wu@ucalgary.ca>
2022-04-11 18:00:02 -0600

    Created event and library classs

---
+++ CPSC_471_Library/Shared/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPSC_471_Library.Shared
+{
+	public class Event
+	{
+		public Event()
+		{
+			public string EventName {get;set;}
+			public int EventId {get;set;}
+		}
+	}
+}
---
+++ CPSC_471_Library/Shared/Library.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPSC_471_Library.Shared
+{
+	public class Library
+	{
+		public Library()
+		{
+			public string LibraryBranch { get; set; }
+			public int LibraryId { get; set; }
+			public string LibraryAddress { get; set; }
+		}
+	}
+}
+
