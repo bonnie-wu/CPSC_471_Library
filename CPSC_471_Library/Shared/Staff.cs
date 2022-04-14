d71673508a57b2dca95e268eaff772e43202785d
joshua.kim1 <joshua.kim1@cs.ucalgary.ca>
2022-04-13 20:22:02 -0600

    added more attributes to staff

--- CPSC_471_Library/Shared/Staff.cs
+++ CPSC_471_Library/Shared/Staff.cs
@@ -11,5 +11,8 @@ namespace CPSC_471_Library.Shared
         public string Name { get; set; } = String.Empty;
         public int Id { get; set; }
         public string Staff_num { get; set; } = String.Empty;
+        public string phone_num { get; set; } = String.Empty;
+        public string position { get; set; } = String.Empty;
+        public string Branch_id { get; set; } = String.Empty;   
     }
 }
