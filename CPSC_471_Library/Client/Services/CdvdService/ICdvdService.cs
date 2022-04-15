37af44d491f8d7a511ede3c3ddf7cd3209ba4e80
30019363 <joshuak803@gmail.com>
2022-04-15 16:07:02 -0600

    services

---
+++ CPSC_471_Library/Client/Services/CdvdService/ICdvdService.cs
@@ -0,0 +1,10 @@
+﻿namespace CPSC_471_Library.Client.Services.CdvdService
+{
+    public interface ICdvdService
+    {
+        Task<List<CDVD>> GetCdvds();
+        Task<CDVD> GetSingleCdvd(int id);
+        Task RemoveCdvd(int id);
+        Task UpdateCdvd(CDVD cdvd);
+    }
+}
