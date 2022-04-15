0524e130ce9aa7b430e4d9d9f04671a1743de7f7
bonnie-wu <bonnie.wu@ucalgary.ca>
2022-04-14 22:22:29 -0600

    Completed Contact Form

---
+++ CPSC_471_Library/Shared/ContactModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPSC_471_Library.Shared
+{
+    public class ContactModel
+    {
+        public string ContactFirstName { get; set; } = string.Empty;
+        public string ContactLastName { get; set; } = string.Empty;
+        public string ContactEmail { get; set; } = string.Empty;
+        public string ContactMessage { get; set; } = string.Empty;
+        public Library ContactLibrary { get; set; }
+
+    }
+}
